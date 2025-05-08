69333e59be83af1803e7ed0bf3dfab9bdfc42cfd
Chaima <93583596+ChaimaElMoussaoui@users.noreply.github.com>
2025-05-08 16:39:44 +0200

    index/css/js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const darkModeButton = document.getElementById("toggle-dark");
+    const darkModeIcon = document.getElementById("dark-mode-icon");
+    let isDarkMode = true;
+
+    darkModeButton.addEventListener("click", () => {
+        isDarkMode = !isDarkMode;
+
+        if (isDarkMode) {
+            document.documentElement.style.setProperty("--bg-color", "var(--bg-color-dark)");
+            document.documentElement.style.setProperty("--text-color", "var(--text-color-dark)");
+            document.documentElement.style.setProperty("--primary-color", "var(--primary-color-dark)");
+            darkModeIcon.textContent = "dark_mode"; // Maan icoon
+        } else {
+            document.documentElement.style.setProperty("--bg-color", "var(--bg-color-light)");
+            document.documentElement.style.setProperty("--text-color", "var(--text-color-light)");
+            document.documentElement.style.setProperty("--primary-color", "var(--primary-color-light)");
+            darkModeIcon.textContent = "light_mode"; // Zon icoon
+        }
+    });
+});
